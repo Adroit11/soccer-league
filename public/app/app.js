5a93b7c563c7ee80a5ea7dea006cfcf9689c48b2
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2015-10-16 16:08:05 -0700

    Create main app.js to create and load Angular/React app

---
+++ public/app/app.js
@@ -0,0 +1,45 @@
+/* app.js
+ * Used to setup app. 
+ * Dependencies: 
+    - modules: angular, angular-route, react, ngReact
+    - components: TeamList, TeamView
+    - services: GetTeams, GetMatches
+    - other: config 
+ * Author: Joshua Carter
+ * Created: October 14, 2015
+ */
+"use strict";
+//include modules
+import angular from 'angular';
+import ngRouteModule from 'angular-route';
+import React from 'react';
+import ngReact from 'ngreact';
+//include components
+import { TeamListController, TeamList } from './league/TeamList.js';
+import { TeamController, TeamView } from './team/TeamView.js';
+//include services
+import { GetTeams } from './shared/GetTeams.js';
+import { GetMatches } from './shared/GetMatches.js';
+//include other objects
+import { config } from './config.js';
+//create app module
+var app = angular.module('GGSoccer', ['react', 'ngRoute'])
+    .config(['$routeProvider', '$locationProvider', config])
+    //create constants
+    .constant('API_ROOT', 'https://futbol-api.goguardian.com')
+    //create services
+    .service('GetTeams', GetTeams)
+    .service('GetMatches', GetMatches)
+    //wrap React components in Angular directives
+    .directive("teamList", function (reactDirective) {
+       return reactDirective(TeamList, undefined, {
+           controller: TeamListController, 
+           controllerAs: 'scope'
+       });
+    })
+    .directive("teamView", function (reactDirective) {
+        return reactDirective(TeamView, undefined, {
+            controller: TeamController,
+            controllerAs: 'scope'   
+        });
+    });
