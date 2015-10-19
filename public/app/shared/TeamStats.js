5a9194a3cd7e41fc12df7de22f3ab0ceb2c601e8
JoshuaCWebDeveloper <coosriverjoshua1@outlook.com>
2015-10-18 22:58:50 -0700

    Create TeamStats React component to display win/loss stats

--- public/app/shared/TeamStats.js
+++ public/app/shared/TeamStats.js
@@ -9,21 +9,16 @@
 import React from 'react';
 //create TeamStats react component
 var TeamStats = class extends React.Component {
-    constructor (props) {
-        //pass props to parent constructor
-        super(props);
-        //create stats
-        this.stats = props.wins + " - " + props.losses;
-        //if there are any ties
-        if (props.ties > 0) {
-            //add ties to stats
-            this.stats += " - " + props.ties;
-        }
-    }
-    
     render () {
+        var stats = this.props.wins + " - " + this.props.losses;
+        //if there are any ties
+        if (this.props.ties > 0) {
+            //add ties to stats
+            stats += " - " + this.props.ties;
+        }
+    
         return (
-            <span>{this.stats}</span>
+            <span>{stats}</span>
         );
     }
 };
